7e3243b9d126ae5c830ee04dfd65c6ca5639dc0c
Bartosz Zaborowski <bartek.zaborowski11@gmail.com>
2023-05-28 18:37:43 +0200

    task1

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,15 @@
+const categories = document.querySelectorAll('li.item')
+console.log(`Number of categories: ${categories.length}`)
 
+const categoryTitle = document.querySelectorAll('h2')
+
+function itemCount() {
+    for (let i = 0; i < categoryTitle.length; i++) {
+        console.log(`Category: ${categoryTitle[i].innerHTML}`)
+        const category = categories[i]
+        const categoryItems = category.querySelectorAll('li')
+        console.log(`Elements: ${categoryItems.length}`)
+    }
+}
+
+console.log(itemCount())
