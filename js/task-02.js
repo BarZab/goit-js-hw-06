101084ce65e65fc67458a1ceed51d8edbf8e996e
Bartosz Zaborowski <bartek.zaborowski11@gmail.com>
2023-05-28 18:57:03 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients')
+
+function addingIngredients() {
+  for (const ingredient of ingredients) {
+    const item = document.createElement('li');
+    item.textContent = ingredient
+    item.classList.add('item')
+    ingredientsList.append(item)
+  }
+}
+
+console.log(addingIngredients())
