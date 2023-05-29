d79e8d3bb65ac46fa54ba3b644fe3e4ac9965c9a
Bartosz Zaborowski <bartek.zaborowski11@gmail.com>
2023-05-30 00:14:06 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('button')
+
+const body = document.querySelector('body')
+
+const colorName = document.querySelector('span.color')
+
+button.addEventListener("click", () => {
+  const color = getRandomHexColor()
+  colorName.innerHTML = color
+  body.style.backgroundColor = color
+});
