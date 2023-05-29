2c0b87fcf273f915b61b3e8a823f96567c120b29
Bartosz Zaborowski <bartek.zaborowski11@gmail.com>
2023-05-29 23:39:58 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return console.log("Please fill in all the fields!");
+    }
+
+    const userObject = {
+        email: email.value,
+        password: password.value
+    }
+
+    console.log(`Login: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+}
 
